2aa464015f3bfd7eb1359e618215fcdeb5bfaacc
M Husain Vijapura <husain.vijapura@gmail.com>
2025-02-18 13:00:59 +0530

    Update backend.js

--- client/src/Components/Util/backend.js
+++ client/src/Components/Util/backend.js
@@ -1,3 +1,3 @@
-// const API_URL = "https://ecoconnect-9x9t.onrender.com"
-const API_URL = "http://localhost:5000";
+const API_URL = "https://ecoconnect-9x9t.onrender.com"
+// const API_URL = "http://localhost:5000";
 export default API_URL;
